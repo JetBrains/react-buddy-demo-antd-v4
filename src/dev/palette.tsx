da81f31807f576517ed98a67a3b525b8601a0f71
Vlad Minaev <minaev@haulmont.com>
2021-10-01 22:37:39 +0400

    Buttons

--- src/dev/palette.tsx
+++ src/dev/palette.tsx
@@ -5,13 +5,69 @@ import {
   Variant,
   Palette,
 } from '@react-buddy/ide-toolbox';
+import {Button} from "antd";
+import {SearchOutlined} from "@ant-design/icons";
 
 export default () => (
   <Palette>
-    <Category name="Text">
-      <Component name="Hello meessage">
+    <Category name="Button">
+      <Component name="Button">
         <Variant>
-          <span> Hello, Create React Buddy App! </span>
+          <Button>Default Button</Button>
+        </Variant>
+        <Variant name="primary">
+          <Button type="primary">Primary Button</Button>
+        </Variant>
+        <Variant name="dashed">
+          <Button type="dashed">Dashed Button</Button>
+        </Variant>
+        <Variant name="text">
+          <Button type="text">Text Button</Button>
+        </Variant>
+        <Variant name="link">
+          <Button type="link">Link Button</Button>
+        </Variant>
+        <Variant name="icon - circle">
+          <Button type="primary" shape="circle" icon={<SearchOutlined />} />
+        </Variant>
+        <Variant name="ghost">
+          <Button ghost>Ghost Default</Button>
+        </Variant>
+        <Variant name="ghost - primary">
+          <Button type="primary" ghost>Ghost Primary</Button>
+        </Variant>
+        <Variant name="ghost - dashed">
+          <Button type="dashed" ghost>Ghost Dashed</Button>
+        </Variant>
+        <Variant name="ghost - link">
+          <Button type="link" ghost>Ghost Link</Button>
+        </Variant>
+        <Variant name="danger">
+          <Button danger>Danger Default</Button>
+        </Variant>
+        <Variant name="danger - primary">
+          <Button type="primary" danger>Danger Primary</Button>
+        </Variant>
+        <Variant name="danger - dashed">
+          <Button type="dashed" danger>Danger Dashed</Button>
+        </Variant>
+        <Variant name="danger - text">
+          <Button type="text" danger>Danger Text</Button>
+        </Variant>
+        <Variant name="danger - link">
+          <Button type="link" danger>Danger Link</Button>
+        </Variant>
+        <Variant name="block">
+          <Button block>Block Default</Button>
+        </Variant>
+        <Variant name="block - primary">
+          <Button type="primary" block>Block Primary</Button>
+        </Variant>
+        <Variant name="block - dashed">
+          <Button type="dashed" block>Block Dashed</Button>
+        </Variant>
+        <Variant name="block - link">
+          <Button type="link" block>Block Link</Button>
         </Variant>
       </Component>
     </Category>
