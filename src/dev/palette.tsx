8676818b680df4b3de93a80f2ba08ae12a4ec550
Vlad Minaev <minaev@haulmont.com>
2021-10-02 12:51:10 +0400

    Typography

--- src/dev/palette.tsx
+++ src/dev/palette.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-  Category,
-  Component,
-  Variant,
-  Palette,
-} from '@react-buddy/ide-toolbox';
-import {Button} from "antd";
+import {Category, Component, Palette, Variant,} from '@react-buddy/ide-toolbox';
 import {SearchOutlined} from "@ant-design/icons";
+import Title from "antd/es/typography/Title";
+import Text from 'antd/es/typography/Text';
+import Link from 'antd/es/typography/Link';
+import Button from 'antd/es/button';
 
 export default () => (
   <Palette>
@@ -68,6 +66,71 @@ export default () => (
         </Variant>
         <Variant name="block - link">
           <Button type="link" block>Block Link</Button>
+        </Variant>
+      </Component>
+    </Category>
+    <Category name="Typography">
+      <Component name="Title">
+        <Variant name="h1">
+          <Title>h1. Ant Design</Title>
+        </Variant>
+        <Variant name="h2">
+          <Title level={2}>h2. Ant Design</Title>
+        </Variant>
+        <Variant name="h3">
+          <Title level={3}>h3. Ant Design</Title>
+        </Variant>
+        <Variant name="h4">
+          <Title level={4}>h4. Ant Design</Title>
+        </Variant>
+        <Variant name="h5">
+          <Title level={5}>h5. Ant Design</Title>
+        </Variant>
+      </Component>
+      <Component name="Text">
+        <Variant>
+          <Text>Ant Design (default)</Text>
+        </Variant>
+        <Variant name="secondary">
+          <Text type="secondary">Ant Design (secondary)</Text>
+        </Variant>
+        <Variant name="success">
+          <Text type="success">Ant Design (success)</Text>
+        </Variant>
+        <Variant name="warning">
+          <Text type="warning">Ant Design (warning)</Text>
+        </Variant>
+        <Variant name="danger">
+          <Text type="danger">Ant Design (danger)</Text>
+        </Variant>
+        <Variant name="disabled">
+        <Text disabled>Ant Design (disabled)</Text>
+        </Variant>
+        <Variant name="mark">
+        <Text mark>Ant Design (mark)</Text>
+        </Variant>
+        <Variant name="code">
+          <Text code>Ant Design (code)</Text>
+        </Variant>
+        <Variant name="keyboard">
+          <Text keyboard>Ant Design (keyboard)</Text>
+        </Variant>
+        <Variant name="underline">
+          <Text underline>Ant Design (underline)</Text>
+        </Variant>
+        <Variant name="delete">
+          <Text delete>Ant Design (delete)</Text>
+        </Variant>
+        <Variant name="strong">
+          <Text strong>Ant Design (strong)</Text>
+        </Variant>
+        <Variant name="italic">
+          <Text italic>Ant Design (italic)</Text>
+        </Variant>
+      </Component>
+      <Component name="Link">
+        <Variant>
+          <Link href="https://ant.design" target="_blank">Ant Design (Link)</Link>
         </Variant>
       </Component>
     </Category>
